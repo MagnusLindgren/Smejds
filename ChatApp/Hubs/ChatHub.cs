c2045490b324fe336bf51185fb6df8e470517962
Daniel Persson <daniel.persson18@learnet.se>
2021-05-27 10:36:53 +0200

    Vi har fått en grupp chat att fungera med hjälp av Singal R

---
+++ ChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+    }
+}
