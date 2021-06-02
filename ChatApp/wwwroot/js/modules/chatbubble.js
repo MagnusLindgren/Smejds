f925c3dc0a8f75ec08361e2a6f26d4feebd4e0ed
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2021-06-02 19:53:23 +0200

    Första upplägget för chatbubblor

---
+++ ChatApp/wwwroot/js/modules/chatbubble.js
@@ -0,0 +1,16 @@
+﻿// Script för att generera chatbubblor
+
+function createChatBubble(message) {
+    const chatbox = document.querySelector(".chat-box");
+
+    const sent = docment.createElement("div");
+    const recived = document.createElement("div");
+    const text = document.createElement("p");
+
+    chatbox.append(sent);
+    sent.append(text);
+
+    text.innerHTML(message);
+
+    sent.classlist.add("sent");
+}
