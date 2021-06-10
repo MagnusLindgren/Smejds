405df528feea16a80656879796b278115547cc8f
Emil Johannesson <Zenatri@gmail.com>
2021-06-10 10:30:04 +0200

    Vi kan nu spara meddelanden + liten if sats som kollar om användaren redan finns i rummet.

--- ChatApp/Hubs/ChatHub.cs
+++ ChatApp/Hubs/ChatHub.cs
@@ -31,6 +31,8 @@ namespace ChatApp.Hubs
             user = Context.User.Identity.Name;
             //await AddToGroup(groupName).ConfigureAwait(false);
             await Clients.OthersInGroup(groupName).SendAsync("ReceiveMessage", user, message);
+
+            await SaveMessagesToDatabase(user, message, groupName);
         }
 
         public Task SendMessageToGroup(string groupName, string user, string message)
@@ -41,11 +43,36 @@ namespace ChatApp.Hubs
         {
             var user = Context.User.Identity.Name;
 
-           // var userInfo = await _userManager.GetUserAsync();
+            await SaveGroupToDatabase(user, groupName);
 
+            var message = $" has joined the group { groupName}.";
+            
+            await Clients.OthersInGroup(groupName).SendAsync("ReceiveMessage", user, message);
+        }
+        
+
+        public async Task SaveMessagesToDatabase(string user, string message, string groupName)
+        {
+            var userTest = await _context.Users.FirstOrDefaultAsync(o => o.UserName == user);
+            ChatRoom test1 = _context.ChatRooms.Include(m => m.Users).FirstOrDefault(o => o.Name == groupName);
+
+            ChatMessage chatMessage = new ChatMessage()
+            {
+                MessageText = message,
+                Users = userTest,
+                Timestamp = DateTime.Now,
+                ChatRoom = test1
+            };
+
+            await _context.ChatMessages.AddAsync(chatMessage);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task SaveGroupToDatabase(string user, string groupName)
+        {
             var userTest = await _context.Users.FirstOrDefaultAsync(o => o.UserName == user);
 
-            await Groups.AddToGroupAsync(Context.ConnectionId, groupName); // Byta ut Connection ID... till en annan User...
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
 
             ChatRoom test1 = _context.ChatRooms.Include(m => m.Users).FirstOrDefault(o => o.Name == groupName);
 
@@ -56,28 +83,26 @@ namespace ChatApp.Hubs
                     Name = groupName,
                     Created_at_date = DateTime.Now,
                     Users = new List<User>() { userTest }
-                    
                 };
                 await _context.ChatRooms.AddAsync(test);
                 await _context.SaveChangesAsync();
             }
             else
             {
-                test1.Users.Add(userTest);
-                await _context.SaveChangesAsync();
+                if (!test1.Users.Contains(userTest))
+                {
+                    test1.Users.Add(userTest);
+                    await _context.SaveChangesAsync();
+                }
             }
-
-       
-
-            var message = $" has joined the group { groupName}.";
-            
-            await Clients.OthersInGroup(groupName).SendAsync("ReceiveMessage", user, message);
-            
         }
-        
 
 
 
-        // Få ut ett unikt COnnectionID från varje användare, sen jämnför användarens ConnectionID med inkomande COnnectionID, så om det stämmer så är det till höger i chaten och är det fel så ska dte läggas till vänster
+
+
+
+
+        // Få ut ett unikt COnnectionID från varje användare, sen jämnför användarens ConnectionID med inkommande COnnectionID, så om det stämmer så är det till höger i chaten och är det fel så ska dte läggas till vänster
     }
 }
