8da7f5e128ad036548b455710e5a9b5519edea06
Emil Johannesson <Zenatri@gmail.com>
2021-06-10 10:46:08 +0200

    Småstädande och borttagning av övergiven vänknapp

--- ChatApp/Models/ChatMessage.cs
+++ ChatApp/Models/ChatMessage.cs
@@ -9,16 +9,9 @@ namespace ChatApp.Models
     public class ChatMessage
     {
         public int Id { get;  set; }
-
         public string MessageText { get; set; }
-
         public DateTime Timestamp { get; set; }
-
         public ChatRoom ChatRoom { get; set; }
-
-
-
         public User Users { get; set; }
-        
     }
 }
--- ChatApp/Models/ChatRoom.cs
+++ ChatApp/Models/ChatRoom.cs
@@ -9,15 +9,9 @@ namespace ChatApp.Models
     public class ChatRoom
     {
         public int Id { get;  set; }
-
         public string Name { get; set; }
         public DateTime Created_at_date { get; set; }
- 
-
-
         public List<ChatMessage> Messages { get; set; }
-
         public List<User> Users { get; set; }
-        
     }
 }
